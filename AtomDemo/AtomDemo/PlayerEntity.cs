3390abad58f41e422f37d643271e80d13234a5a3
Piefarmer <chris.normansell@gmail.com>
2015-01-10 01:40:47 +0000

    Added keybindings to common components of PlayerEntity

--- AtomDemo/AtomDemo/PlayerEntity.cs
+++ AtomDemo/AtomDemo/PlayerEntity.cs
@@ -4,6 +4,7 @@ using Atom.Entity;
 using Atom.Graphics;
 using Atom.Input;
 using Atom.Physics;
+using Microsoft.Xna.Framework.Input;
 
 namespace AtomDemo
 {
@@ -13,10 +14,46 @@ namespace AtomDemo
         {
             List<Component> components = new List<Component>
             {
-                new PositionComponent(),
-                new VelocityComponent(),
-                new AnimatedSpriteComponent(),
-                new FirstPersonKeyComponent()
+                new PositionComponent() { EntityId = Id },
+                new VelocityComponent() { EntityId = Id },
+                new AnimatedSpriteComponent() { EntityId = Id },
+
+                new FirstPersonKeyComponent() { 
+                    EntityId = Id, 
+                    Action = FirstPersonKeyComponent.FirstPersonActions.Up, 
+                    Key = Keys.W
+                },
+
+                new FirstPersonKeyComponent() { 
+                    EntityId = Id, 
+                    Action = FirstPersonKeyComponent.FirstPersonActions.Down, 
+                    Key = Keys.S
+                },
+
+                new FirstPersonKeyComponent() { 
+                    EntityId = Id, 
+                    Action = FirstPersonKeyComponent.FirstPersonActions.Left, 
+                    Key = Keys.A
+                },
+
+                new FirstPersonKeyComponent() { 
+                    EntityId = Id, 
+                    Action = FirstPersonKeyComponent.FirstPersonActions.Right, 
+                    Key = Keys.D
+                },
+
+                new FirstPersonKeyComponent() { 
+                    EntityId = Id, 
+                    Action = FirstPersonKeyComponent.FirstPersonActions.Fire, 
+                    Key = Keys.Space
+                },
+
+                new FirstPersonKeyComponent() { 
+                    EntityId = Id, 
+                    Action = FirstPersonKeyComponent.FirstPersonActions.AltFire, 
+                    Key = Keys.Q
+                }
+
             };
 
             return components;
