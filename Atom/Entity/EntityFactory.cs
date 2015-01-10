25f89fb74c0a80c98ced449265a64d1fe31db9e9
Piefarmer <chris.normansell@gmail.com>
2015-01-10 02:18:52 +0000

    Updated EntityFactory to have a few choices on how to add a EntityType

--- Atom/Entity/EntityFactory.cs
+++ Atom/Entity/EntityFactory.cs
@@ -8,7 +8,7 @@ namespace Atom.Entity
     {
         private static EntityFactory _instance = new EntityFactory();
 
-        private HashSet<Type> _entityTypes = new HashSet<Type>();
+        private Dictionary<string, Type> _entityTypes = new Dictionary<string, Type>();
 
         private int _currentEntityId = 0;
 
@@ -17,34 +17,61 @@ namespace Atom.Entity
         /// <summary>
         /// Registers an entity type that can be used to create an instance of that entity
         /// </summary>
-        /// <param name="type"></param>
-        public void RegisterEntity(Type type)
+        /// <param name="id">The id that you want to set the entity to</param>
+        /// <param name="type">The type of entity</param>
+        public void RegisterEntity(string id, Type type)
         {
             if (!type.IsSubclassOf(typeof (Entity)))
             {
                 throw new NotEntityException();
             }
 
-            _entityTypes.Add(type);
+            if (_entityTypes.ContainsKey(id))
+            {
+                return;
+            }
+
+            _entityTypes.Add(id, type);
+        }
+
+        /// <summary>
+        /// Registers an entity type that can be used to create an instance of that entity. It will create an id from the FullName of the type.
+        /// </summary>
+        /// <param name="type"></param>
+        public void RegisterEntity(Type type)
+        {
+            RegisterEntity(type.FullName, type);
+        }
+
+        /// <summary>
+        /// Registers an entity type that can be used to create an instance of that entity
+        /// </summary>
+        /// <typeparam name="T">The type of entity you want to register</typeparam>
+        /// <param name="id">The id to give the entity type you are registering</param>
+        public void RegisterEntity<T>(string id)
+        {
+            RegisterEntity(id, typeof (T));
+        }
+
+        /// <summary>
+        /// Registers an entity type that can be used to create an instance of that entity. It will create an id from the FullName of the type.
+        /// </summary>
+        /// <typeparam name="T">The type of entity you want to register. Will use type FullName as the id.</typeparam>
+        public void RegisterEntity<T>()
+        {
+            RegisterEntity<T>(typeof (T).FullName);
         }
 
         /// <summary>
         /// Creates a entity from the type given. The type needs to be registered before it can be created.
         /// </summary>
-        /// <typeparam name="T">The type of entity that will be created</typeparam>
+        /// <param name="id">The id of the entity</param>
         /// <returns>Returns a new instance of the Entity</returns>
-        public T CreateEntity<T>() where T : Entity
+        public Entity CreateEntity(string id)
         {
-            IEnumerable<Type> types = _entityTypes.Where(type => type.FullName == typeof (T).FullName);
+            Type entityType = _entityTypes[id];
 
-            var enumerable = types as IList<Type> ?? types.ToList();
-
-            if (!enumerable.Any())
-            {
-                return null;
-            }
-
-            var entity = Activator.CreateInstance(enumerable.First()) as Entity;
+            var entity = Activator.CreateInstance(entityType) as Entity;
 
             if (entity == null)
             {
@@ -53,7 +80,40 @@ namespace Atom.Entity
 
             entity.Id = GetNextEntityId();
 
-            return entity as T;
+            return entity;
+        }
+
+        /// <summary>
+        /// Creates a entity from the type given. The type needs to be registered before it can be created.
+        /// </summary>
+        /// <param name="type">The type of entity you want. Will use FullName as id.</param>
+        /// <returns>Returns a new instance of the Entity or null if it fails to do so</returns>
+        public Entity CreateEntity(Type type)
+        {
+            return CreateEntity(type.FullName);
+        }
+
+        /// <summary>
+        /// Creates a entity from the type given. The type needs to be registered before it can be created.
+        /// </summary>
+        /// <typeparam name="T">The type you want the Entity to be returned as</typeparam>
+        /// <param name="id">The id of the entity</param>
+        /// <returns>Returns a new instance of the Entity or null if it fails to do so</returns>
+        public T CreateEntity<T>(string id) where T : Entity
+        {
+            var entity = CreateEntity(id);
+
+            return (T) entity;
+        }
+
+        /// <summary>
+        /// Creates a entity from the type given. The type needs to be registered before it can be created.
+        /// </summary>
+        /// <typeparam name="T">The type you want the Entity to be returned as</typeparam>
+        /// <returns>Returns a new instance of the Entity or null if it fails to do so</returns>
+        public T CreateEntity<T>() where T : Entity
+        {
+            return CreateEntity<T>(typeof (T).FullName);
         }
 
         /// <summary>
--- AtomDemo/AtomDemo/Game1.cs
+++ AtomDemo/AtomDemo/Game1.cs
@@ -1,14 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using Atom;
 using Atom.Entity;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 
 namespace AtomDemo
 {
@@ -38,13 +34,13 @@ namespace AtomDemo
 
             EntityFactory entityFactory = EntityFactory.GetInstance();
 
-            entityFactory.RegisterEntity(typeof (PlayerEntity));
+            EntityFactory.GetInstance().RegisterEntity<PlayerEntity>();
 
             PlayerEntity entity = entityFactory.CreateEntity<PlayerEntity>();
             PlayerEntity entity1 = entityFactory.CreateEntity<PlayerEntity>();
 
-            Console.WriteLine(entity.CreateCommonComponents());
-            Console.WriteLine(entity1.CreateCommonComponents());
+            List<Component> components = entity.CreateDefaultComponents();
+            Console.WriteLine(entity1.CreateDefaultComponents());
 
             base.Initialize();
         }
