6c490a37b1d4370fc1ffe4900b4423fec1ea04a3
jazz-e <ritchiejohnson1970@gmail.com>
2015-01-16 13:18:46 +0000

    Add type filter class

---
+++ Atom/TypeFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atom
+{
+    public class TypeFilter
+    {
+        private List<Type> _filters = new List<Type>();
+
+        public TypeFilter()
+        {
+
+        }
+
+        public TypeFilter(List<Type> filters)
+        {
+            _filters = filters;
+        }
+
+        public TypeFilter AddFilter(Type type)
+        {
+            _filters.Add(type);
+
+            return this;
+        }
+
+        public bool Contains(Type type)
+        {
+            return _filters.FindAll(filterType => filterType.FullName == type.FullName).Count > 0;
+        }
+    }
+}
