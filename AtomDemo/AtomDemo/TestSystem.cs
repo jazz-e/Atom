e21fa8452a635e3e054d5dad5a589aea812e21e8
Chris Stroud <chris.stroud00@gmail.com>
2015-01-16 16:37:48 +0000

    Updated TestSystem to match System abstract

--- AtomDemo/AtomDemo/TestSystem.cs
+++ AtomDemo/AtomDemo/TestSystem.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using Atom.Messaging;
+using Atom.Physics;
 using Microsoft.Xna.Framework;
 using Atom;
 using Atom.Graphics;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace AtomDemo
 {
     public class TestSystem : Atom.System, IReceiver
     {
         public int Id { get; set; }
+        public List<PositionComponent> PositionComponents;  
+        public List<VelocityComponent> VelocityComponents;  
 
         public TestSystem()
         {
@@ -21,7 +25,7 @@ namespace AtomDemo
             PostOffice.SendMessage(new NewMessage());
         }
 
-        public TypeFilter GetTypeFilter()
+        public TypeFilter GetMessageTypeFilter()
         {
             return new TypeFilter()
                 .AddFilter(typeof(NewMessage));
@@ -30,6 +34,51 @@ namespace AtomDemo
         public void OnMessage(IMessage message)
         {
             Console.WriteLine(message.Data[0]);
+        }
+
+        public override void Update(GameTime gameTime, int entityId)
+        {
+            
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, int entityId)
+        {
+            
+        }
+
+        public override void Purge<T>(int entityId)
+        {
+            
+        }
+
+        public override void Disable<T>(int entityId)
+        {
+            
+        }
+
+        public override void Enable<T>(int entityId)
+        {
+            
+        }
+
+        public override void RemoveEntityComponents(int entityId)
+        {
+            PositionComponents.RemoveAll(component => component.EntityId == entityId);
+            VelocityComponents.RemoveAll(component => component.EntityId == entityId);
+        }
+
+        public override void AddComponent(Component component)
+        {
+            if (component.GetType() == typeof (PositionComponent))
+            {
+                PositionComponents.Add((PositionComponent)component);
+            }
+            else if (component.GetType() != typeof (VelocityComponent))
+            {
+                VelocityComponents.Add((VelocityComponent)component);                
+            }
+
+
         }
     }
 
