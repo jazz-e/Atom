0f11cfccec84a8513a69a71f9abee86f970d1641
Piefarmer <chris.normansell@gmail.com>
2015-01-18 22:47:55 +0000

    Added Standard Keyboard system. Resolves #9

---
+++ Atom/Input/StandardKeyComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace Atom.Input
+{
+    public class StandardKeyComponent : Component
+    {
+        /// <summary>
+        /// The action to be initiated when Key is pressed
+        /// </summary>
+        public StandardInputActions Action { get; set; }
+
+        /// <summary>
+        /// The key to trigger the action defined by Action
+        /// </summary>
+        public Keys Key { get; set; }
+    }
+}
---
+++ Atom/Input/StandardKeyboardSystem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Atom.Messaging;
+using Atom.Physics.Movement;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Atom.Input
+{
+    public class StandardKeyboardSystem : BaseSystem
+    {
+        public StandardKeyboardSystem()
+        {
+            ComponentTypeFilter = new TypeFilter()
+                .AddFilter(typeof(StandardKeyComponent));
+        }
+
+        public override void Update(GameTime gameTime, int entityId)
+        {
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            List<StandardKeyComponent> keyComponents = GetComponentsByEntityId<StandardKeyComponent>(entityId);
+
+            foreach (StandardKeyComponent keyComponent in keyComponents)
+            {
+                if (keyboardState.IsKeyUp(keyComponent.Key)) continue;
+
+                StandardInputActions actions = keyComponent.Action;
+
+                switch (actions)
+                {
+                    case StandardInputActions.Up:
+                        PostOffice.SendMessage(new MoveMessage(entityId, MoveDirection.Up));
+                        break;
+                    case StandardInputActions.Down:
+                        PostOffice.SendMessage(new MoveMessage(entityId, MoveDirection.Down));
+                        break;
+                    case StandardInputActions.Left:
+                        PostOffice.SendMessage(new MoveMessage(entityId, MoveDirection.Left));
+                        break;
+                    case StandardInputActions.Right:
+                        PostOffice.SendMessage(new MoveMessage(entityId, MoveDirection.Right));
+                        break;
+                    case StandardInputActions.Fire:
+                        break;
+                    case StandardInputActions.AltFire:
+                        break;
+                }
+            }
+        }
+    }
+}
